ad3584195265d81f39f620565ea3c77525f9ebc4
monsalex23 <monsalex23@gmail.com>
2022-06-22 11:45:30 -0300

    React practice componentizing an app that consumes api from github

---
+++ src/components/Layaout/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Layaout() {
+    return (
+        <section>
+            <header>
+                Hola
+            </header>
+            {}
+        </section>
+    );
+}
+
+export default Layaout
